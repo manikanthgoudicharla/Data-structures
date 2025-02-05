ce90f2be1c526e85bc1a93c9e9a162d5939b2707
gmy282 <goudicharlamanikanth88@gmail.com>
2025-02-05 20:13:16 +0530

    update bubble sort

--- arrays/bubblesort.c
+++ arrays/bubblesort.c
@@ -1,19 +1,41 @@
 #include<stdio.h>
 
-void sortarray(){
-    printf("sort");
+void bubblesort(int size,int array[]){
+ int i,j,temp;
+ for(i=0;i<size;i++){
+     for(j=0;j<size-i-1;j++){
+         if(array[j]>array[j+1]){
+             temp=array[j];
+             array[j]=array[j+1];
+             array[j+1]=temp;
+         }
+         else{
+             continue;
+         }
+     }
+ }
 }
-void printarray(){
 
-}
-int main(){
-    int n;
-    printf("Enter The size of the Array:");
-    scanf("%d",&n);
-    int array[n],i;
-    for(i=0;i<n;i++){
-        scanf("Enter %d Th element of the array:",&array[i]);
+void printarray(int size,int array[]){
+    int i;
+      for(i=0;i<size;i++){
+        printf("the %d th element of the array is:%d\n",i+1,array[i]);
     }
+}
 
+int main(){
+    int size;
+    printf("Enter the Size of the Array:");
+    scanf("%d",&size);
+    int array[size],i;
+    for(i=0;i<size;i++){
+        printf("Enter %d th Element of the Array:",i+1);
+        scanf("%d",&array[i]);
+    }
+    printf("Array Before Bubble sort:\n");
+    printarray(size,array);
+    bubblesort(size,array);
+    printf("Array After Bubble sort:\n");
+    printarray(size,array);
 
 }
